625ad35ce3e9866ace196e58f5ce06105a561153
Roshan Gurung <roshangurung125@gmail.com>
2022-06-05 21:08:51 +0545

    Final

--- TODO/mainapp/urls.py
+++ TODO/mainapp/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('home/' , views.home, name = 'home'),
     path('', views.home),
     path('add/', views.add, name = 'add'),
-    path('edit/<int:id>', views.edit, name = 'edit')
+    path('edit/<int:id>', views.edit, name = 'edit'),
+    path('delete/<int:id>', views.delete, name = 'delete'),
+    path('delete-all/', views.delete_all, name = 'delete-all'),
+    path('mark-as-complete/<int:id>', views.mark_as_complete, name = 'mark-as-complete')
 ]
--- TODO/mainapp/views.py
+++ TODO/mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from mainapp.models import TODO
 # Create your views here.
 
@@ -10,9 +10,41 @@ def home(request):
     return render(request, 'home.html', context)
 
 def add(request):
-    return render(request, 'add.html')
+    if request.method == 'GET':
+        return render(request, 'add.html')
+    else:
+        title = request.POST['title']
+        desc = request.POST['desc']
+
+        TODO.objects.create(title = title, desc = desc)
+
+        return redirect('home')    
 
 def edit(request, id):
     todo = TODO.objects.get(id=id)
-    context = {'todo':todo}
-    return render(request, 'edit.html', context)
+    if request.method == 'GET':
+        context = {'todo':todo}
+        return render(request, 'edit.html', context)
+    else:
+        todo.title = request.POST['title']
+        todo.desc = request.POST['desc']
+
+        todo.save()
+        return redirect('home')
+    
+
+def delete(request, id):
+    todo = TODO.objects.get(id = id)
+    todo.delete()
+    return redirect('home')
+
+def delete_all(request):
+    TODO.objects.all().delete()
+    return redirect('home')
+
+def mark_as_complete(request, id):
+    todo = TODO.objects.get(id = id)
+
+    todo.is_completed = not todo.is_completed
+    todo.save()
+    return redirect('home')
